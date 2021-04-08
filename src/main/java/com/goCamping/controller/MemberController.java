e90b6109286dc8263b6468f04b6a28bf1a6c51a9
SbinSho <suho0232@gmail.com>
2021-04-09 00:24:26 +0900

    공통된 반복작업 ( 개인키, 공개키 생성 ) 제거를 위한, 키 생성 클래스 KeySey.java 생성, MemberController 적용. MemberController if 문 처리 구조 조금 개선

--- src/main/java/com/goCamping/controller/MemberController.java
+++ src/main/java/com/goCamping/controller/MemberController.java
@@ -19,17 +19,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.goCamping.dto.MemberLoginDTO;
 import com.goCamping.dto.MemberJoinDTO;
+import com.goCamping.dto.MemberLoginDTO;
 import com.goCamping.service.EncryptService;
-import com.goCamping.service.MailService;
 import com.goCamping.service.MemberService;
-import com.goCamping.validator.MemberLoginValidator;
+import com.goCamping.util.KeySet;
 import com.goCamping.validator.MemberJoinDTOValidator;
+import com.goCamping.validator.MemberLoginValidator;
 
 @Controller
 @RequestMapping("/member")
@@ -41,9 +39,6 @@ public class MemberController {
 	private MemberService member_service;
 	
 	@Autowired
-	private MailService mail_service;
-	
-	@Autowired
 	private EncryptService encrypt_service;
 	
 	
@@ -53,31 +48,29 @@ public class MemberController {
 		
 		logger.info("join GET 요청");
 		
-		// 현재 세션 가져오기
-		HttpSession session = request.getSession();
-
 		// 개인키 공개키 생성
 		Map<String, Object> map = encrypt_service.createKey();
 		
-		// 생성된 개인키 및 공개키가 존재하지 않으면 null
-		if (map != null) {
-			
-			session.setAttribute("_RSA_WEB_KEY_", map.get("_RSA_WEB_KEY_"));
-			request.setAttribute("RSAModulus", map.get("RSAModulus"));
-			request.setAttribute("RSAExponent", map.get("RSAExponent"));
-			request.setAttribute("memberJoinDTO", new MemberJoinDTO());
-			
-			
+		
+		if(map != null) {
+			KeySet.set(request, map);
 			return "/member/join";
 		}
 		
 		rttr.addFlashAttribute("result", "error");
-		
-		
 		return "redirect:/";
-		
 	}
 	 
+	
+	public void keySet(HttpServletRequest request, Map<String, Object> map) {
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("_RSA_WEB_KEY_", map.get("_RSA_WEB_KEY_"));
+		request.setAttribute("RSAModulus", map.get("RSAModulus"));
+		request.setAttribute("RSAExponent", map.get("RSAExponent"));
+		
+	}
+	
 	// 회원가입 처리
 	@RequestMapping( value="/join", method = RequestMethod.POST )
 	public String join(MemberJoinDTO memberJoinDTO, BindingResult bindingResult, HttpServletRequest request, RedirectAttributes rttr) throws Exception{
@@ -108,15 +101,14 @@ public class MemberController {
 				// 복호화된 객체 유효성 체크
 				new MemberJoinDTOValidator().validate(memberJoinDTO, bindingResult);
 				
+				
 				// 유효성에 문제가 발생하면 새로운 요청을 통해 회원가입 페이지로 이동
 				if(bindingResult.hasErrors()) {
 					
 					logger.info("객체 유효성 검증 실패!");
-					
-//					rttr.addFlashAttribute("result", "error");
-//					return "redirect:/";
-					
-					return "/member/join";
+					logger.info(bindingResult.getAllErrors());
+					rttr.addFlashAttribute("result", "error");
+					return "redirect:/";
 					
 				}
 				
@@ -130,14 +122,16 @@ public class MemberController {
 				} else {
 					
 					// 현재 DB에서 중복체크 모두 완료 후, 데이터 입력이 되지 않고 오류가 발생할 경우
-					rttr.addFlashAttribute("result", "error");
 					logger.info("회원가입 오류 발생!");
+					rttr.addFlashAttribute("result", "error");
 				}
 				
-			} else {
-				// 암호문 복호화 실패
-				rttr.addFlashAttribute("result", "error");
+				return "redirect:/";
 			}
+			
+			// 암호문 복호화 실패
+			logger.info("암호문 복호화 실패!");
+			rttr.addFlashAttribute("result", "error");
 			
 			return "redirect:/";
 		}
@@ -154,18 +148,13 @@ public class MemberController {
 		
 		logger.info("login GET 요청");
 		
-		// 현재 세션 가져오기
-		HttpSession session = request.getSession();
-
 		// 개인키 공개키 생성
 		Map<String, Object> map = encrypt_service.createKey();
 		
 		// 생성된 개인키 및 공개키가 존재하지 않으면 null
 		if (map != null) {
 			
-			session.setAttribute("_RSA_WEB_KEY_", map.get("_RSA_WEB_KEY_"));
-			request.setAttribute("RSAModulus", map.get("RSAModulus"));
-			request.setAttribute("RSAExponent", map.get("RSAExponent"));
+			keySet(request, map);
 			
 			// 브라우저에 저장된 cookie 체크
 			if(cookie != null) {
@@ -181,7 +170,7 @@ public class MemberController {
 		rttr.addFlashAttribute("result", "error");		
 		return "redirect:/";
 	}
-	
+
 	// 로그인 처리
 	@RequestMapping( value="/login", method = RequestMethod.POST )
 	public void login(MemberLoginDTO memberLoginDTO, BindingResult bindingResult, HttpServletRequest request, Model model) throws Exception{
@@ -191,11 +180,11 @@ public class MemberController {
 		// 현재 세션 가져오기
 		HttpSession session = request.getSession();
 		// 세션에 저장된 개인키를 가져오기
-		PrivateKey privateKey = (PrivateKey) session.getAttribute("_RSA_WEB_KEY_");	
+		PrivateKey privateKey = (PrivateKey) session.getAttribute("_RSA_WEB_KEY_");
 		
 		// 개인키가 존재하는지 체크
 		if(privateKey != null) {
-			
+			// memberLoginDTO 필드에 저장된 값들을 저장할 String 배열
 			String[] memberLoginDTO_Encrypt_Array = { 
 					memberLoginDTO.getUser_id(), memberLoginDTO.getUser_pwd(), memberLoginDTO.getUser_reId() };
 			
@@ -234,13 +223,12 @@ public class MemberController {
 				model.addAttribute("check", "fail");
 			}
 			
-			
-		} else {
-			// 개인키가 존재하지 않을 경우
-			model.addAttribute("private", "private_error");
 			return;
 		}
 		
+		// 개인키가 존재하지 않을 경우
+		model.addAttribute("private", "private_error");
+		return;
 	}
 	
 	//로그아웃 처리
@@ -266,77 +254,11 @@ public class MemberController {
 		
 	}
 	
-	
-	// 아이디 중복 확인
-	@RequestMapping( value = "/idCheck", method = RequestMethod.POST )
-	@ResponseBody
-	public int id_Check( String user_id ) throws Exception{
+	// 회원정보 수정 페이지 이동
+	@RequestMapping( value = "/edit", method = RequestMethod.GET)
+	public String edit() {
 		
-		logger.info("/idCheck 요청");
-		
-		// DB에 user_id 컬럼은 현재 기본키로 잡혀있음, 그러므로 아이디 중복 시 반환값은 1 아닐 경우 0 
-		return member_service.id_Check(user_id);
-		
+		return "/member/edit";
 	}
-	
-	// 닉네임 중복 확인
-	@RequestMapping( value = "/nickCheck", method = RequestMethod.POST )
-	@ResponseBody
-	public int nick_Check( String user_nickname ) throws Exception{
-		
-		logger.info("/nickCheck 요청");
-		
-		// DB에 user_nickname은 기본키이다, 그러므로 아이디 중복시 반환값은 1 아닐 경우 0
-		return member_service.nick_Check(user_nickname);
-	}
-	
-	// 메일 중복 확인
-	@RequestMapping( value = "/mailCheck", method = RequestMethod.POST)
-	@ResponseBody
-	public int mail_Check( String user_mail, HttpSession session) throws Exception {
-		
-		logger.info("/mailCheck 요청");
-		
-		int mail_result = -1;
-		
-		mail_result = mail_service.mail_Check(user_mail);
-		
-		// mail_result가 1인 경우, 중복된 메일이 존재하는 경우
-		// 인증코드 전송 작업을 하지 않고 현재 mail_result 값을 바로 리턴함
-		if(mail_result == 1) {
-			return mail_result;
-		}
-		
-		// 사용자에게 인증코드 전송하고, 전송된 인증코드를 MailService에서 반환받음
-		String auth_key = mail_service.sendAuthMail(user_mail);
-		// 반환받은 인증코드를 세션에 저장함
-		session.setAttribute("auth_key", auth_key);
-		
-		
-		return mail_result; 
-	}
-	
-	// 메일 인증코드 확인 처리
-	@RequestMapping( value = "/authCheck", method = RequestMethod.POST)
-	@ResponseBody
-	public int auth_check(@RequestParam("auth_code") String auth_code, HttpSession session) {
-		
-		int auth_result = 0;
-		
-		// 현재 세션에 저장된 인증코드를 가져온다
-		String ses_key = (String) session.getAttribute("auth_key");
-		
-		// 세션에 저장된 인증코드와 사용자에게 입력된 인증코드를 비교한다.
-		if(ses_key.equals(auth_code)) {
-			
-			// 인증코드가 같으면 현재 세션에 저장되어있는 인증코드를 제거한다.
-			session.removeAttribute("auth_key");
-			auth_result = 1;
-			
-		}
-		
-		return auth_result;
-	}
-	
 	
 }
---
+++ src/main/java/com/goCamping/util/KeySet.java
@@ -0,0 +1,19 @@
+package com.goCamping.util;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class KeySet {
+	
+	public static void set(HttpServletRequest request, Map<String, Object> map) {
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("_RSA_WEB_KEY_", map.get("_RSA_WEB_KEY_"));
+		request.setAttribute("RSAModulus", map.get("RSAModulus"));
+		request.setAttribute("RSAExponent", map.get("RSAExponent"));
+		
+	}
+
+}
