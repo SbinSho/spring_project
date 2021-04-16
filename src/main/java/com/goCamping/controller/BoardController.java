355589be95b718e4e09560fe24b431ac2ceb08ae
SbinSho <suho0232@gmail.com>
2021-04-16 18:56:42 +0900

    BoardController 및 Mybatis setup

--- src/main/java/com/goCamping/controller/BoardController.java
+++ src/main/java/com/goCamping/controller/BoardController.java
@@ -1,13 +1,19 @@
 package com.goCamping.controller;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.goCamping.domain.BoardVO;
+import com.goCamping.domain.Criteria;
+import com.goCamping.domain.PageMaker;
+import com.goCamping.service.BoardService;
 
 @Controller
 @RequestMapping("/board")
@@ -15,10 +21,22 @@ public class BoardController {
 	
 	private static final Logger logger = LogManager.getLogger(BoardController.class);
 
+	@Autowired
+	private BoardService board_service;
+	
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String notice() {
+	public String notice(Criteria cri, Model model) {
 		
-		logger.info("/question GET 호출");
+		logger.info("/list GET 호출");
+		
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(board_service.BoardCount());
+		
+		List<BoardVO> list = board_service.selectBoardList(cri);
+		
+		model.addAttribute("list", list);
+		model.addAttribute("pageMaker", pageMaker);
 		
 		return "/board/list";
 	}
